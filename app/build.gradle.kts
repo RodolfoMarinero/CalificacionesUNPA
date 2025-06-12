751346573d04ac9440544e4c06315924bc0401a0
AnyOleaH27 <anyoh2003@gmail.com>
2025-06-12 09:43:00 -0600

    Avances de notificacioness

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.androidx.datastore.core.android)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
