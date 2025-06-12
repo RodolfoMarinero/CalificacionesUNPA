79246db9277026943ff4b10460021fef2f1b7371
SaulJM27 <saul.jums03@gmail.com>
2025-06-12 10:27:03 -0600

    avances calendario escolar

--- app/src/main/java/mx/edu/unpa/calificacionesunpa/ui/calendarioescolar/FragmentCalendarioEscolar.kt
+++ app/src/main/java/mx/edu/unpa/calificacionesunpa/ui/calendarioescolar/FragmentCalendarioEscolar.kt
@@ -4,44 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
-import com.github.barteksc.pdfviewer.PDFView
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
 import mx.edu.unpa.calificacionesunpa.R
-import java.io.File
 
 class FragmentCalendarioEscolar : Fragment() {
 
-    private lateinit var pdfView: PDFView
+    private lateinit var webView: WebView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val view = inflater.inflate(R.layout.fragment_calendario_escolar, container, false)
-        pdfView = view.findViewById(R.id.pdfView)
-        cargarPdfDesdeFirebase()
+
+        webView = view.findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        val fileId = "1qAMJ70t0CcKNHu6XoFDyfWbeIDQ4wQR2"
+        val pdfUrl = "https://drive.google.com/file/d/1rILxFFcWXNrkyZ8Kz0pRVobb0a6Z05zi/view?usp=sharing"
+        webView.loadUrl(pdfUrl)
+
         return view
-    }
-
-    private fun cargarPdfDesdeFirebase() {
-        val storageRef = Firebase.storage.reference
-        val pdfRef = storageRef.child("calendario_escolar_2024_2025.pdf") // Cambia el nombre según tu archivo en Firebase
-
-        val localFile = File.createTempFile("tempPdf", "pdf")
-
-        pdfRef.getFile(localFile)
-            .addOnSuccessListener {
-                pdfView.fromFile(localFile)
-                    .enableSwipe(true)
-                    .swipeHorizontal(false)
-                    .enableDoubletap(true)
-                    .load()
-            }
-            .addOnFailureListener { e ->
-                Toast.makeText(requireContext(), "Error al cargar el PDF: ${e.message}", Toast.LENGTH_LONG).show()
-            }
     }
 }
